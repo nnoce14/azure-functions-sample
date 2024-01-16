3acdd544f7c3b07b711cc2ae8b0e159f0ddeb751
Duy Nguyen <nkduy2010@gmail.com>
2024-01-16 13:46:08 -0500

    add upload on frontend

--- ui/src/http-blobstorage.tsx
+++ ui/src/http-blobstorage.tsx
@@ -1,27 +1,58 @@
-import { Button, Upload, UploadProps, message } from "antd";
-import { FC } from "react";
 import { UploadOutlined } from "@ant-design/icons";
+import { Button, Upload, UploadFile, UploadProps, message } from "antd";
+import { FC, useState } from "react";
 
 interface HttpBlobstorageProps {}
 export const HttpBlobstorage: FC<HttpBlobstorageProps> = (props) => {
+  const [fileList, setFileList] = useState<UploadFile<any>[]>([]);
+
+  const customUploadRequest = async (options: any) => {
+    const { file, filename } = options;
+    const formData = new FormData();
+    console.log("filename", filename);
+
+    formData.append("uploadedFile", file);
+    try {
+      const response = await fetch(`http://localhost:7071/api/http/upload-file/${file.name}`, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.status !== 200) {
+        message.error("Fail to upload");
+        return;
+      }
+      message.success("File uploaded successfully");
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
 
   const uploadProps: UploadProps = {
     name: "file",
-    action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188", //TODO: upload api
-    onChange(info) {
-      if (info.file.status !== "uploading") {
-        console.log(info.file, info.fileList);
+    onChange: (info) => {
+      if (info.file.status === "uploading") {
+        console.log("uploading", info.file, info.fileList);
+        return
       }
       if (info.file.status === "done") {
         message.success(`${info.file.name} file uploaded successfully`);
+        setFileList([]);
       } else if (info.file.status === "error") {
         message.error(`${info.file.name} file upload failed.`);
       }
     },
+    beforeUpload: (file) => {
+      console.log("beforeUpload - file", file);
+      setFileList([file]);
+      return true;
+    },
+    customRequest: customUploadRequest,
   };
+
   return (
     <>
-      <Upload {...uploadProps}>
+      <Upload {...uploadProps} fileList={fileList}>
         <Button icon={<UploadOutlined />}>Click to Upload</Button>
       </Upload>
     </>
