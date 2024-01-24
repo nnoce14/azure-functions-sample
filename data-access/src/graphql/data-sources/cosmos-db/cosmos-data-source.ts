dac3e95d37d9ec5daaff208441778fcd4c09728b
Nick Noce <nnoce12@gmail.com>
2024-01-24 12:53:38 -0500

    Created data sources and added cosmos data source for data models

---
+++ data-access/src/graphql/data-sources/cosmos-db/cosmos-data-source.ts
@@ -0,0 +1,12 @@
+
+import { MongoDataSource, MongoDataSourceConfig } from 'apollo-datasource-mongodb';
+import { Context as GraphQLContext } from '../../context';
+
+export class CosmosDataSource<TData, Context extends GraphQLContext> extends MongoDataSource<TData> {
+    protected context: Context;
+  
+    constructor(options: { context: Context } & MongoDataSourceConfig<TData>) {
+      super(options);
+      this.context = options.context;
+    }
+  }
