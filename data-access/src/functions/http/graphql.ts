08461bcfe4dbb89e1cd91502ce9739376b09f26a
Nick Noce <nnoce12@gmail.com>
2024-01-24 12:52:51 -0500

    Created context class and added it to apollo server

--- data-access/src/functions/http/graphql.ts
+++ data-access/src/functions/http/graphql.ts
@@ -1,9 +1,22 @@
-import { HttpHandler, app } from "@azure/functions";
-import { server } from "../../graphql/init/apollo";
+import { app } from "@azure/functions";
+import { ApolloServerRequestHandler } from "../../graphql/init/apollo";
 import { startServerAndCreateHandler } from "../../graphql/init/func-v4";
+import { Context as ApolloContext } from "../../graphql/context";
+
+let apolloServerRequestHandler = new ApolloServerRequestHandler(
+    new Map<string, string>([
+        ["AccountPortal", "ACCOUNT_PORTAL"]
+    ])
+);
 
 app.http("graphql", {
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"],
     route: "graphql/{*segments}",
-    handler: startServerAndCreateHandler(server),
+    handler: startServerAndCreateHandler<ApolloContext>(apolloServerRequestHandler.getServer(), {
+        context: async ({ req }) => {
+            let context = new ApolloContext();
+            context.init(req, apolloServerRequestHandler);
+            return context;
+        }
+    }),
 });
